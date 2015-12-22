e8082ecc8501e74ec2e87087f825e76e80624cb5
velikiinyashko <velikiinyashko@gmail.com>
2015-12-22 13:34:14 +0700

    mail sender
    add phpmailer
    recoding mail send for smtp
    test file - send.php, in index.php - send to engl leng

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 			settings,
 			settings)
 	</script>-->
-	<form enctype="multipart/form-data" action="sendmail.php" method="POST">
+	<form enctype="multipart/form-data" action="parse.php" method="POST">
 		<input type="hidden" name="MAX_FILE_SIZE" value="600000"/>
 		Загрузите *.csv для отправки на русском языке: <input name="maillist" type="file"/>
 		<input type="submit" value="Отправить"/>
 	</form>
 	<a href="file_send_mail.csv">результат отправки</a>
-		<form enctype="multipart/form-data" action="sendmailen.php" method="POST">
+		<form enctype="multipart/form-data" action="send.php" method="POST">
 		<input type="hidden" name="MAX_FILE_SIZE" value="600000"/>
 		Загрузите *.csv для отправки на английском языке: <input name="maillist" type="file"/>
 		<input type="submit" value="Отправить"/>
--- parse.php
+++ parse.php
@@ -1,7 +1,22 @@
 <?php
-$homepage = file_get_contents('http://24whois.ru/?data=3dmodeli.net&t=whois');
-$find = 'Admin Email:';
-$stroka = strpos($homepage, $find);
+$maillist=$_FILES['maillist']['tmp_name'];
 
-print_r($stroka);
+
+$file=fopen($maillist, "r") or die ("Warning!");
+for ($i=0; $i=fgetcsv($file, 1000, ";"); $i++) { 
+	list($site) = $i;
+	//$adminmail = file_get_contents('http://24whois.ru/?data='.$site.'&t=whois');
+	$adminmail = file_get_contents('https://www.reg.ru/whois/?dname='.$site.'&_csrf=b56d505b61035182861d357f9c44336e');
+		$mailadmin = preg_match('/([a-z0-9_\.\-])+\@(([a-z0-9\-])+\.)+([a-z0-9]{2,4})+/i', $adminmail, $findmail);
+		$ipadd = preg_match('/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]/', $adminmail, $ip);
+	//$mailabuse = file_get_contents('http://24whois.ru/?t=whois&data=$ip[0]');
+		//$mail = preg_match('/([a-z0-9_\.\-])+\@(([a-z0-9\-])+\.)+([a-z0-9]{2,4})+/i', $mailabuse, $abuse);
+		$contact = preg_match('~(http|https|ftp|ftps)://(.*?)(\s|\n|[,.?!](\s|\n)|$)~', $adminmail, $conaddr);
+			print_r($findmail[0]);
+			print_r($ip[0]);
+			print_r($conaddr[0]);
+			//print_r($abuse[0]);
+}
+
+
 ?>
