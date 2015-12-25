d98cad559161a6f6b5697445053b9a76320c2f8b
velikiinyashko <velikiinyashko@gmail.com>
2015-12-25 16:29:27 +0700

    v0.005a

--- parse.php
+++ parse.php
@@ -4,7 +4,7 @@ $maillist=$_FILES['maillist']['tmp_name'];
 $file=fopen($maillist, "r+") or die ("Warning!");
 for ($i=0; $info=fgetcsv($file, 1000, ";"); $i++) { 
 	list($site) = $info;
-	$adminmail = file('http://192.168.0.1/whois/index.php?domain='.$site);
+	$adminmail = file('http://89.105.144.180/whois/index.php?domain='.$site);
 	$ipadd = preg_match('/[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/', $adminmail[0], $ip);
 	$abuse = file('http://2whois.ru/?t=whois&data='.$ip[0]);
 	$mail = preg_match('/([a-z0-9_\.\-])+\@(([a-z0-9\-])+\.)+([a-z0-9]{2,4})+/i', $abuse[244], $mailto);
